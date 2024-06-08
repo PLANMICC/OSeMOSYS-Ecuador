bd5513d406427ae7f49d8418972246534312751e
PLANMICC <151056269+PLANMICC@users.noreply.github.com>
2024-06-07 19:04:18 -0600

    Versión del modelo de Energía, sin TCE

---
+++ Energia/t1_confection_SIN_TCE/A1_Outputs/check_fleet.py
@@ -0,0 +1,8 @@
+import pickle
+
+# infile = open('A-O_Fleet_Groups.pickle','rb')
+# new_dict = pickle.load(infile)
+# infile.close()
+
+new_dict = pickle.load(open('A-O_Fleet_Groups.pickle','rb'))
+new_dict_2 = pickle.load(open('A-O_Fleet_Groups_TRANSPORT.pickle','rb'))
