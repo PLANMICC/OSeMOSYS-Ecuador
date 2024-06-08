32c0044fffadf5fc7f904164148a1c1fe6cea5dc
PLANMICC <151056269+PLANMICC@users.noreply.github.com>
2024-06-07 18:31:44 -0600

    Delete Energia/Model/t1_confection directory
    Se elimina para cargar la versión con tren de carga eléctrico y sin tren de carga eléctrico

--- Energia/Model/t1_confection/A1_Outputs/check_fleet.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-# infile = open('A-O_Fleet_Groups.pickle','rb')
-# new_dict = pickle.load(infile)
-# infile.close()
-
-new_dict = pickle.load(open('A-O_Fleet_Groups.pickle','rb'))
-new_dict_2 = pickle.load(open('A-O_Fleet_Groups_TRANSPORT.pickle','rb'))
